ea60742d25c35b2022d866c787c8884def00ce98
khizxr <khizharrana@gmail.com>
2023-11-07 14:29:57 +0000

    Practical 1 completed

---
+++ practical1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python 3
+
+print("1 program has executed")
+print("2 program executed")
+print("3rd program executed")
+
+help(print)
+"""
+quit()
+"""
+print(10 + 20 - 15)
+print(10 + 20 - 15)
+print(100 / 33)
+print(100 // 33)
+print(10 ** 2)
+print(10 % 3)
+print(10 + 5 * 2)
+print(10 - 5 * 10 + 5)
+print(5 * 10 ** 2)
+print(12 + (5 * 2 + 3))
+print(12 + (5 * (2 + 3)))
+print("Ten divided by zero is", 10 / 0)
+
